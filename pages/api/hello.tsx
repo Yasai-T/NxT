42922f4fc8e078eff377b0b9e45e5c83d9b0f940
tomoki midorikawa <46930312+Yasai-T@users.noreply.github.com>
2020-08-27 20:33:02 +0900

    prettier eslint config

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -1,11 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+const handler = (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  res.statusCode = 200;
+  res.json({ name: "John Doe" });
+};
+
+export default handler;
